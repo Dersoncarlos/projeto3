999d3230f356121ef85aa79a6eb2acc7cea0825c
dersoncarlos <dersoncarlos@gmail.com>
2023-03-16 22:49:33 -0300

    chore: database migrations

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = \Faker\Factory::create('pt_BR');
+        return [
+            'full_name' => $faker->name(),
+            'mother_full_name' => $faker->name("female"),
+            'birthday' => $faker->date(),
+            'cpf' => $faker->cpf(),
+        ];
+    }
+}
---
+++ database/migrations/2023_03_17_011617_create_patients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        //         Foto do Paciente;
+        // Nome Completo do Paciente;
+        // Nome Completo da Mãe;
+        // Data de Nascimento;
+        // CPF;
+        // SNC;
+        // Endereço completo, (CEP, Endereço, Número, Complemento, Bairro, Cidade e Estado)*;
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string("photo")->nullable();
+            $table->string("full_name");
+            $table->string("mother_full_name");
+            $table->string("birthday");
+            $table->string("cpf");
+            $table->string("snc")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
